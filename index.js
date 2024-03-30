6f031a035100092d43d3c44dbb8f6dfb077ad453
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-03-30 11:56:39 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const body = document.body
+
+const main = document.querySelector('main#main')
+main.remove()
+//body.append(main)
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+//body.append(newHeader)
